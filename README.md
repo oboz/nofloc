7b64017de3ed4e091df91e77fe4b8a9b4736c8bd
Andrei Abozau <d3wayorg@gmail.com>
2021-04-29 04:38:57 +0300

    Add middleware

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # nofloc
 Middleware to block Google FLoC tracking in Express or Koa
+
+## Install
+
+Run `npm install nofloc --save` to add package.
+
+## Usage for Express
+```js
+const { noflocExpress } = require("nofloc");
+
+// some code...
+
+app.use(noflocExpress());
+```
+
+## Usage for Koa
+```js
+const { noflocKoa } = require("nofloc");
+
+// some code...
+
+app.use(noflocKoa());
+```
+
+
---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const name = "permissions-policy";
+const rule = "interest-cohort=()";
+
+function noflocExpress() {
+  return function(req, res, next) {
+    let header = rule;
+    if (res.hasHeader(name)) {
+      header = res.getHeader(name);
+      if (header.indexOf(rule) === -1) {
+        header += ', ' + rule;
+      }
+    }
+    res.setHeader(name, header);
+    next();
+  }
+}
+
+async function noflocKoa() {
+  return async function(ctx, next) {
+    let header = ctx.response.header[name] || rule;
+    if (header.indexOf(rule) === -1) {
+      header += ', ' + rule;
+    }
+    ctx.set(name, header);
+    await next();
+  }
+}
+
+module.exports = {
+  noflocExpress,
+  noflocKoa
+}
